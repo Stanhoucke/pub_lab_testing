cd3447f1df56ba19fcf737ed9079b28930e2bd42
Stanley Houcke <stanhoucke@gmail.com>
2020-11-25 14:14:40 +0000

    finished mvp

--- src/pub.py
+++ src/pub.py
@@ -4,5 +4,17 @@ class Pub():
         self.till = till
         self.drinks = drinks
 
-    def sell_a_drink(self, drink):
-        pass
+    def add_money_to_till(self, cash):
+        self.till += cash
+
+    def find_drink_by_name(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink
+
+    def buy_a_drink(self, customer, drink_name):
+        if self.find_drink_by_name(drink_name) != None:
+            order = self.find_drink_by_name(drink_name)
+            if customer.can_afford(order) == True:
+                customer.remove_money_from_wallet(order.price)
+                self.add_money_to_till(order.price)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.pub import Pub
 from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     def setUp(self):
@@ -10,10 +11,14 @@ class TestPub(unittest.TestCase):
         self.fosters = Drink("Fosters", 4.00)
         self.carling = Drink("Carling", 4.50) 
         self.guiness = Drink("Guiness", 5.95)
+        self.shiraz = Drink("Shiraz", 7.95)
         
         stock = [self.tennents, self.stella, self.fosters, self.carling, self.guiness]
         
         self.pub = Pub("The Prancing Pony", 100.00, stock)
+
+        self.customer_1 = Customer("Dave", 50.00)
+        self.customer_2 = Customer("Freddy", 0.50)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -24,3 +29,30 @@ class TestPub(unittest.TestCase):
     def test_pub_has_stock(self):
         self.assertEqual(5, len(self.pub.drinks))
 
+    def test_add_money_to_till(self):
+        cash = 5.50
+        self.pub.add_money_to_till(cash)
+        self.assertEqual(105.50, self.pub.till)
+
+    def test_find_drink_by_name(self):
+        order_1 = self.tennents.name
+        order_2 = self.shiraz.name
+        self.assertEqual(self.tennents, self.pub.find_drink_by_name(order_1))
+        self.assertEqual(None, self.pub.find_drink_by_name(order_2))
+
+    def test_buy_a_drink(self):
+        # Arrange
+        customer_1 = self.customer_1
+        customer_2 = self.customer_2
+
+        order_1 = self.tennents.name
+        order_2 = self.shiraz.name
+        # Act
+        self.pub.buy_a_drink(customer_1, order_1)
+        self.pub.buy_a_drink(customer_2, order_2)
+
+        # Assert
+
+        self.assertEqual(45.05, self.customer_1.wallet)
+        self.assertEqual(0.50, self.customer_2.wallet)
+        self.assertEqual(104.95, self.pub.till)
