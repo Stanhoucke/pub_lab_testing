77dab478aebd3fe75ecdf6da5fefb69d24db6721
Stanley Houcke <stanhoucke@gmail.com>
2020-11-25 11:52:09 +0000

    passed init test for customer

--- src/drink.py
+++ src/drink.py
@@ -1,5 +1,5 @@
 class Drink():
-    
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,6 +1,12 @@
 import unittest
 from src.customer import Customer
 
-class TestCustomer():
+class TestCustomer(unittest.TestCase):
     def setUp(self):
-        pass
+        self.customer = Customer("Dave", 50.00)
+
+    def test_customer_has_name(self):
+        self.assertEqual("Dave", self.customer.name)
+
+    def test_customer_has_money(self):
+        self.assertEqual(50.00, self.customer.wallet)
